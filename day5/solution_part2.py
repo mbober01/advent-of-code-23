123a783c20a1818d642e0a859d5f92307a5f672f
mbober01 <98718462+mbober01@users.noreply.github.com>
2023-12-06 00:09:04 +0100

    part 2 SOLVED!!!

--- day5/solution_part2.py
+++ day5/solution_part2.py
@@ -39,7 +39,12 @@ class Almanac:
         self.mappers.append(mapper)
 
     def find_lowest_location(self):
-        return min([seed.loc for seed in self.seeds])
+        min_ranges = []
+        for seed in self.seeds:
+            for s_range in seed.ranges:
+                min_ranges.append(s_range.start)
+
+        return min(min_ranges)
 
 
 class Seed:
@@ -61,6 +66,8 @@ class SingleMap:
         self.source_range_start = int(self.instruction[0])
         self.dest_range_start = int(self.instruction[1])
         self.range_len = int(self.instruction[2])
+        self.source_r = range(self.source_range_start, self.source_range_start + self.range_len)
+        self.dest_r = range(self.dest_range_start, self.dest_range_start + self.range_len)
 
     def __repr__(self):
         return (f"SingleMap(dest_range_start={self.dest_range_start}, source_range_start={self.source_range_start},"
@@ -70,19 +77,57 @@ class SingleMap:
 class Mapper:
     def __init__(self):
         self.name = ""
-
         self.maps: List[SingleMap] = []
 
     def __repr__(self):
         return f"Mapper(name={self.name}, maps={self.maps})"
 
     def map_seed(self, seed: Seed):
-        for single_map in self.maps:
-            seed_dist = seed.loc - single_map.dest_range_start
-            if single_map.range_len > seed_dist >= 0:
-                return single_map.source_range_start + seed_dist
+        leave_alone = []
+        def split_range(seed_r, dest_r, source_r):
+            new_ranges: List[range] = []
+            old_ranges: List[range] = []
+            start_dis = abs(seed_r.start - dest_r.start)
+            end_dis = abs(seed_r.stop - dest_r.stop)
 
-        return seed.loc
+            if seed_r.start > dest_r.stop or seed_r.stop < dest_r.start:
+                old_ranges.append(seed_r)
+
+            elif seed_r.start < dest_r.start and seed_r.stop <= dest_r.stop:
+                old_ranges.append(range(seed_r.start, dest_r.start-1)) # 1
+                new_ranges.append(range(source_r.start, source_r.stop - end_dis))
+
+            elif seed_r.start >= dest_r.start and seed_r.stop <= dest_r.stop:
+                new_ranges.append(range(source_r.start + start_dis, source_r.stop - end_dis))
+
+            elif seed_r.start >= dest_r.start and seed_r.stop > dest_r.stop:
+                new_ranges.append(range(source_r.start + start_dis, source_r.stop))
+                old_ranges.append(range(dest_r.stop+1, seed_r.stop))
+
+            elif seed_r.start < dest_r.start and seed_r.stop > dest_r.stop:  # moga byc zle znaki
+                new_ranges.append(range(source_r.start, source_r.stop))
+                old_ranges = [range(seed_r.start, dest_r.start-1), range(dest_r.stop+1, seed_r.stop)]
+
+            else:
+                print("else")
+
+            return new_ranges, old_ranges
+
+        for single_map in self.maps:
+            still_map = []
+            for seed_range in seed.ranges:
+                mapped_ranges_new, not_mapped_ranges_new = split_range(seed_range, single_map.dest_r, single_map.source_r)
+                leave_alone.extend(mapped_ranges_new)
+                still_map.extend(not_mapped_ranges_new)
+
+            if still_map:
+                seed.ranges = still_map
+            else:
+                break
+
+        merged = leave_alone + still_map
+        seed.ranges = merged
+
 
 
 def main():
@@ -91,14 +136,12 @@ def main():
     almanac.get_maps()
 
     for seed in almanac.seeds:
-
-        # for mapper in almanac.mappers:
-        #     new_loc = mapper.map_seed(seed)
-        #     seed.move_seed(new_loc, mapper.name)
-
         print(seed)
+        for mapper in almanac.mappers:
+            mapper.map_seed(seed)
+            print("mapper done")
 
-    # print(f"Lowest location number is: {almanac.find_lowest_location()}")
+    print(f"Lowest location number is: {almanac.find_lowest_location()}")
 
 
 if __name__ == '__main__':
